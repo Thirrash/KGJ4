edeaed5c360f5935f7e0b53954746dfd40d5c40a
DESKTOP-DCF12DK\PPESN <zbednyemaildozalozeniakontafb@gmail.com>
2017-09-16 20:02:37 +0200

    Added basic functionality for menu.

---
+++ Assets/Scripts/LoadGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadGame : MonoBehaviour {
+
+    public void LoadMainScene()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void LoadMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
