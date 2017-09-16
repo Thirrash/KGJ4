d98ef164790a55703d9e98bbeee10a4f4759bb3e
DESKTOP-DCF12DK\PPESN <zbednyemaildozalozeniakontafb@gmail.com>
2017-09-16 22:25:50 +0200

    Created Timer responsible for measuring time till end of game.

---
+++ Assets/Scripts/ReturnToMenuManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToMenuManager : MonoBehaviour {
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.Escape))
+            Application.LoadLevel(0);
+    }
+}
---
+++ Assets/Scripts/TimerManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour {
+
+    public Text ResultText;
+
+    private Text TimerText;
+    private float timeLeft = 15.0f;
+    private bool bIsFinished = false;
+
+    public float minScore = 4.76f;
+
+    void Start()
+    {
+        ResultText.text = "";
+        TimerText = GetComponent<Text>();
+        StartCoroutine(UpdateText());
+    }
+
+    private void Update()
+    {
+        if (bIsFinished) { return; }
+
+        timeLeft -= Time.deltaTime;
+
+        if(timeLeft <= 0)
+        {
+            StopCoroutine(UpdateText());
+            if (CostManager.Instance.TotalCost > minScore)
+            {
+                Win();
+            }
+            else
+            {
+                Lose();
+            }
+
+            bIsFinished = true;
+        }
+    }
+
+    private IEnumerator UpdateText()
+    {
+        while (true)
+        {
+            int time = (int)timeLeft;
+            int minutes = time / 60;
+            int seconds = time - minutes * 60;
+
+            TimerText.text = minutes.ToString("D2") + ":" + seconds.ToString("D2");
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    private void Win() { ResultText.text = "!You win!\n\nPress Escape to return to Menu"; }
+
+    private void Lose() { ResultText.text = "!You lose!\n\nPress Escape to return to Menu"; }
+}
